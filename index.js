f61bc9e5fa4fa2b2c0acab1c4ae447d6a28477ae
Kazantsev Gera <113335397+Ubersmurf2010@users.noreply.github.com>
2023-09-06 12:27:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ app.patch("/cards/:id", checkAuth, cardController.update);
 app.get("/cards/", cardController.getAll);
 app.get("/cards/:id", cardController.getOne);
 
-app.listen(80, (err) => {
+app.listen(8000, (err) => {
   if (err) {
     return console.log(err);
   }
